3eeec832e2d2b9366d56d46cac737dc47e3e2bf4
hangfeng <332393900@qq.com>
2026-04-15 14:54:09 +0800

    fix: 优化样式

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -7,15 +7,6 @@ import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return null
-  }
 
   return (
     <Button
